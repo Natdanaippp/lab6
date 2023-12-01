1ab795b9960f24fa5ab564309e888078dd37b344
ppp <natdanai_kamthorn@cmu.ac.th>
2023-12-01 15:06:59 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int number=999;
+    int even =0;
+    int odd =0;
+    while(number!=0)
+    {
+        cout << "Enter an integer: ";
+        cin >> number;
+        if(number%2==0&&number!=0)
+        {
+            even+=1;
+        }
+        if(number%2==1&&number!=0)
+        {
+            odd+=1;
+        }
+    }
+    cout << "#Even numbers = "<<even<<endl;
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
